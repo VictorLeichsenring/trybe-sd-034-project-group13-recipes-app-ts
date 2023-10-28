398a525b7e27fb8e9b387e03bf94f77c8dec1cdf
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-10-28 04:00:41 -0300

    correções lint

--- src/components/RecommendationCard/index.tsx
+++ src/components/RecommendationCard/index.tsx
@@ -2,7 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './RecommendationCard.css';
 
-function RecommendationCard({ recommendation, index, isMeal }) {
+type RecommendationType = {
+  strDrinkThumb?: string;
+  strMealThumb?: string;
+  strDrink?: string;
+  strMeal?: string;
+};
+
+function RecommendationCard({
+  recommendation,
+  index,
+  isMeal,
+}: {
+  recommendation: RecommendationType;
+  index: number;
+  isMeal: boolean;
+}) {
   return (
     <div
       className="recommendation-card"
@@ -20,7 +35,12 @@ function RecommendationCard({ recommendation, index, isMeal }) {
 }
 
 RecommendationCard.propTypes = {
-  recommendation: PropTypes.object.isRequired,
+  recommendation: PropTypes.shape({
+    strDrinkThumb: PropTypes.string,
+    strMealThumb: PropTypes.string,
+    strDrink: PropTypes.string,
+    strMeal: PropTypes.string,
+  }).isRequired,
   index: PropTypes.number.isRequired,
   isMeal: PropTypes.bool.isRequired,
 };
