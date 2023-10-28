98b83b0a231623c9b48eb252fd4115bedf0ee060
Hugo Leon <hugoleon@outlook.com>
2023-10-27 21:38:20 -0300

    requisito 20 lint-refeito

--- src/components/RecipeProvider.tsx
+++ src/components/RecipeProvider.tsx
@@ -3,13 +3,11 @@ import RecipeContext from '../context/RecipeContext';
 
 function RecipeProvider({ children }: { children: React.ReactNode }) {
   const [recipes, setRecipes] = useState([]);
-  const [categories, setCategories, // Função para atualizar os ingredi
-  ] = useState([]);
+
   const providerValue = {
     recipes,
     setRecipes,
-    categories, // Adicionado estado de ingredientes
-    setCategories, // Função para atualizar os ingredi
+
   };
 
   return (
