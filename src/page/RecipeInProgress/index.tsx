8927ad428dc9714b106330a4018cb9167bf207ce
João <joaovitorlisboasilva@gmail.com>
2023-10-30 13:08:19 -0300

    testes do Recipe (thiago )e desenvolvendo a 31 (Victor)

---
+++ src/page/RecipeInProgress/index.tsx
@@ -0,0 +1,7 @@
+function RecipeInProgress() {
+  return (
+    <h1>Recipe In Progress page </h1>
+  );
+}
+
+export default RecipeInProgress;
--- src/tests/Recipes.test.tsx
+++ src/tests/Recipes.test.tsx
@@ -19,4 +19,21 @@ describe('Recipes Component', () => {
     const link = getByTestId('0-recipe-link');
     expect(link).toHaveAttribute('href', '/meals/testId');
   });
+  test('renders a drink recipe correctly', async () => {
+    const { getByTestId } = renderWithRouter(<Recipes
+      image="testImage"
+      name="testName"
+      index={ 1 }
+      id="testId"
+      type="drink"
+    />);
+    const recipeCard = getByTestId('1-recipe-card');
+    expect(recipeCard).toBeInTheDocument();
+    const image = getByTestId('1-card-img');
+    expect(image).toHaveAttribute('src', 'testImage');
+    const name = getByTestId('1-card-name');
+    expect(name).toHaveTextContent('testName');
+    const link = getByTestId('1-recipe-link');
+    expect(link).toHaveAttribute('href', '/drinks/testId');
+  });
 });
