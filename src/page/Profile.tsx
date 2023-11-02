127a780ff20b774070e002094f416bb8ee2a81d6
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-11-02 03:45:35 -0300

    Criada funções de navegar, e logout com limpeza do localstorage

--- src/page/Profile.tsx
+++ src/page/Profile.tsx
@@ -1,16 +1,38 @@
+import { useNavigate } from 'react-router-dom';
 import { getFromLocalStorage } from '../services/localStorageUtils';
 
 function Profile() {
+  const navigate = useNavigate();
   const user = getFromLocalStorage('user', '');
   const { email } = user;
+
+  function handleLogout() {
+    localStorage.clear();
+    navigate('/');
+  }
 
   return (
     <div>
       <h1>Profile</h1>
       <p data-testid="profile-email">{ email }</p>
-      <button data-testid="profile-done-btn">Done Recipes</button>
-      <button data-testid="profile-favorite-btn">Favorite Recipes</button>
-      <button data-testid="profile-logout-btn">Logout</button>
+      <button
+        data-testid="profile-done-btn"
+        onClick={ () => navigate('/done-recipes') }
+      >
+        Done Recipes
+      </button>
+      <button
+        data-testid="profile-favorite-btn"
+        onClick={ () => navigate('/favorite-recipes') }
+      >
+        Favorite Recipes
+      </button>
+      <button
+        data-testid="profile-logout-btn"
+        onClick={ handleLogout }
+      >
+        Logout
+      </button>
     </div>
   );
 }
