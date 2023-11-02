c59e8711c185262004f623b1e9cd574261680201
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-11-02 03:38:43 -0300

    Criado tela profile com email e 3 botões

--- src/page/Profile.tsx
+++ src/page/Profile.tsx
@@ -1,7 +1,16 @@
+import { getFromLocalStorage } from '../services/localStorageUtils';
+
 function Profile() {
+  const user = getFromLocalStorage('user', '');
+  const { email } = user;
+
   return (
     <div>
-      <h1>Meals</h1>
+      <h1>Profile</h1>
+      <p data-testid="profile-email">{ email }</p>
+      <button data-testid="profile-done-btn">Done Recipes</button>
+      <button data-testid="profile-favorite-btn">Favorite Recipes</button>
+      <button data-testid="profile-logout-btn">Logout</button>
     </div>
   );
 }
